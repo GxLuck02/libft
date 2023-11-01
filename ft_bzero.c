bfa059f6f9f4eb1a40175f38fd8bd1759075d300
ttreichl <ttreichl@student.42lausanne.ch>
2023-11-01 17:50:18 +0100

    test

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ttreichl <ttreichl@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 18:06:12 by ttreichl          #+#    #+#             */
-/*   Updated: 2023/10/31 13:03:10 by ttreichl         ###   ########.fr       */
+/*   Updated: 2023/11/01 17:49:56 by ttreichl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,4 +14,5 @@
 void	ft_bzero(void *ptr, size_t nbroct)
 {
 	ft_memset(ptr, 0, nbroct);
+	return;
 }
